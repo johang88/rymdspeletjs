4bb09981bd39875fec242fd94582ec89d63c94d1
Johan Gustafsson <johan@tinyrocket.se>
2019-03-03 18:53:22 +0100

    initial spritebatch work

--- src/rse/SpriteBatch.ts
+++ src/rse/SpriteBatch.ts
@@ -6,4 +6,36 @@ namespace rse {
         texture:WebGLTexture;
         flags:number;
     }
+
+    export enum TextAlignment {
+        Left,
+        Right,
+        Center
+    }
+
+    export class SpriteBatch {
+        private sprites:SpriteInfo[];
+
+        private vertexBuffer:WebGLBuffer;
+
+        constructor() {
+            this.vertexBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        }
+
+        drawSprite(sprite:Sprite) {
+        }
+
+        drawColoredRect(rect:Rect, color:Color) {
+        }
+
+        drawTexture(rect:Rect, uv:Rect, color:Color, texture:Texture) {
+        }
+
+        // todo: print methods here ...
+
+        // Submit pending draw calls
+        submit(width:number, height:number, shaderFlags:number = -1) {
+        }
+    }
 }
